fe84224a5dd722e89dfd00b846fd1ab54c9f9717
sergii <davygorasergii@gmail.com>
2023-12-10 14:33:43 -0800

    aleart_iframe

---
+++ src/test/java/com/loop/test/day6_alearts_iframes/T01_alert.java
@@ -0,0 +1,122 @@
+package com.loop.test.day6_alearts_iframes;
+
+import com.loop.test.base.TestBase;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class T01_alert extends TestBase {
+
+     /*
+    Information alert practice
+    1. Open browser
+    2. Go to website: https://loopcamp.vercel.app/javascript-alerts.html
+    3. Click to “Click for JS Alert” button
+    4. Click to OK button from the alert
+    5. Validate “You successfully clicked an alert” text is displayed
+
+    Confirmation alert practice
+    1. Click to “Click for JS Confirm” button
+    2. Click to OK button from the alert
+    3. Verify “You clicked: Ok” text is displayed.
+    4. Click to “Click for JS Confirm” button
+    5. Click to Cancel button from the alert
+    6. Validate “You clicked: Cancel” text is displayed.
+
+    Prompt alert practice
+    1. Click to “Click for JS Prompt” button
+    2. Send "Loop Academy"
+    3. Click Ok
+    4. Validate "You entered: Loop Academy" text is displayed
+
+
+     */
+
+
+
+
+
+    @Test
+    public  void javaAlert(){
+        driver.get("https://loopcamp.vercel.app/javascript-alerts.html");
+        WebElement clickForJS = driver.findElement(By.xpath(" //button[text()='Click for JS Alert']"));
+        clickForJS.click();
+
+
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        WebElement successMassageForInformation = driver.findElement(By.xpath("//p[@id='result']"));
+        String actual = "You successfully clicked an alert";
+        String expected = successMassageForInformation.getText();
+        Assert.assertEquals(actual,expected,"TEST FAIL");
+
+    }
+
+
+    @Test
+    public void confirmationAlert(){
+        driver.get("https://loopcamp.vercel.app/javascript-alerts.html");
+        WebElement clickFroJSConfirm = driver.findElement(By.xpath("//button[@onclick='jsConfirm()']"));
+        clickFroJSConfirm.click();
+        driver.switchTo().alert().accept();
+        WebElement successMassageForInformation = driver.findElement(By.xpath("//p[@id='result']"));
+        String expected = "You clicked: Ok";
+        String actual =  successMassageForInformation.getText();
+        Assert.assertEquals(actual,expected,"TEST FAIL");
+
+        clickFroJSConfirm.click();
+        driver.switchTo().alert().accept();
+        actual = successMassageForInformation.getText();
+        expected = "You clicked: Ok";
+        Assert.assertEquals(actual,expected,"TEST FAIL");
+
+
+        clickFroJSConfirm.click();
+        driver.switchTo().alert().dismiss();
+        actual = successMassageForInformation.getText();
+        expected = "You clicked: Cancel";
+        Assert.assertEquals(actual,expected,"TEST FAIL");
+
+
+
+    }
+
+
+    @Test
+    public void promptAlert() throws InterruptedException {
+        driver.get("https://loopcamp.vercel.app/javascript-alerts.html");
+        WebElement clickJSPrompt = driver.findElement(By.xpath("//button[contains(text(),'Click for JS Prompt')]"));
+        clickJSPrompt.click();
+        String text = "Loop Academy";
+//        driver.switchTo().alert().sendKeys(text);
+//        driver.switchTo().alert().accept();
+        Alert alert = driver.switchTo().alert();
+        alert.sendKeys(text);
+        alert.accept();
+
+        WebElement successMessagePromptAlert = driver.findElement(By.xpath("//p[@id='result']"));
+        String actual = successMessagePromptAlert.getText();
+        String expected = "You entered: " + text;
+        Assert.assertEquals(actual, expected, "actual does not match the expected");
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
--- src/test/java/com/loop/test/utilities/DocuportConstants.java
+++ src/test/java/com/loop/test/utilities/DocuportConstants.java
@@ -2,7 +2,7 @@ package com.loop.test.utilities;
 
 public class DocuportConstants {
 
-    public static final String USERNAME_CLIENT = "b1g1_client@gmail.com";
+    public static final String USERNAME_CLIENT = "b1g3_client@gmail.com";
 
     public static final String ERROR_MASSAGE_FOR_EMPTY_PASSWORD = "Please enter  your password";
 
@@ -13,6 +13,8 @@ public class DocuportConstants {
     public  static  final String RESET_PASSWORD_MASSAGE = "Enter the email address associated with your account";
 
     public  static  final String EMAIL_FOR_RESET_PASSWORD = "forgotpasswordg1@gmail.com";
+    public static final String USERNAME_ADVISOR = "b1g3_advisor@gmail.com";
+    public static final String PASSWORD_DOCUPORT = "Group3";
 
 
 
