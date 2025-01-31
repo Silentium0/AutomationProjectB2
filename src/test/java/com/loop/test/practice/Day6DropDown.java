02c11d38b7a52e8c7959f44ae4a7be4d348d3ebb
sergii <davygorasergii@gmail.com>
2025-01-30 21:51:46 -0500

    Window_Hendel

--- src/test/java/com/loop/test/practice/Day6DropDown.java
+++ src/test/java/com/loop/test/practice/Day6DropDown.java
@@ -55,6 +55,7 @@ public class Day6DropDown {
 
 
 
+
     }
 
 
--- src/test/java/com/loop/test/practice/Login.java
+++ src/test/java/com/loop/test/practice/Login.java
@@ -1,15 +1,19 @@
 package com.loop.test.practice;
 
+import com.loop.test.base.TestBase;
 import com.loop.test.utilities.DocuportConstants;
+import com.loop.test.utilities.DocuportUtils;
 import com.loop.test.utilities.Driver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
-public class Login {
+import java.time.Duration;
+
+public class Login extends TestBase {
 
     @Test
-    public void ligin(){
+    public void login(){
         Driver.getDriver().get("https://beta.docuport.app/login?redirect=%2F");
         WebElement userName = Driver.getDriver().findElement(By.xpath("//label[text()='Username or email']/following-sibling::input"));
         WebElement password = Driver.getDriver().findElement(By.xpath("//label[text()='Password']/following-sibling::input"));
@@ -18,5 +22,22 @@ public class Login {
         userName.sendKeys(DocuportConstants.USERNAME_CLIENT);
         password.sendKeys(DocuportConstants.PASSWORD_DOCUPORT);
         login.click();
+    }
+    @Test
+    public void liginAndOut() throws InterruptedException {
+        DocuportUtils.login(driver,"advisor");
+        DocuportUtils.logOut(driver);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        DocuportUtils.login(driver,"supervisor");
+        DocuportUtils.logOut(driver);
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        DocuportUtils.login(driver,"employee");
+        DocuportUtils.logOut(driver);
+
+
     }
 }
